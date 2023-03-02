1a1887969bc2600e118083539abeb002931f3d1d
Matheus Sales <matheus.carvalho.sales@gmail.com>
2023-03-02 09:03:13 -0300

    feat: add checkbox

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,5 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
 
 export const NewHabitForm = () => (
   <form className=" w-full flex flex-col mt-6">
@@ -15,8 +16,20 @@ export const NewHabitForm = () => (
     />
 
     <label htmlFor="" className="font-semibold leading-tight mt-4">
-      How many times per week do you want to do this habit?
+      What days do you want to do this habit?
     </label>
+
+    <div className="flex flex-col gap-2 mt-3">
+      <Checkbox.Root className="flex items-center gap-3 group">
+        <div className="w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+          <Checkbox.Indicator>
+            <Check size={20} className="text-white" />
+          </Checkbox.Indicator>
+        </div>
+
+        <span className="text-white leading-tight">Monday</span>
+      </Checkbox.Root>
+    </div>
 
     <button
       type="submit"
