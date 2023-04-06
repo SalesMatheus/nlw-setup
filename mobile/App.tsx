db31b32cb3e917183378286cf63efb5375864a40
Matheus Sales <matheus.carvalho.sales@gmail.com>
2023-04-06 08:16:58 -0300

    feat: add push notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import "./src/lib/dayjs";
-
+import * as Notifications from "expo-notifications";
 import { StatusBar } from "react-native";
+
 import {
   useFonts,
   Inter_400Regular,
@@ -12,6 +13,16 @@ import {
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    };
+  },
+});
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -19,6 +30,26 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold,
   });
+
+  const scheduleNotification = async () => {
+    console.log("scheduleNotification");
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Hey, 🥰",
+        body: "You've got mail! 📬",
+      },
+      trigger,
+    });
+  };
+
+  const getScheduledNotifications = async () => {
+    const notifications =
+      await Notifications.getAllScheduledNotificationsAsync();
+    console.log(notifications);
+  };
 
   if (!fontsLoaded) {
     return <Loading />;
